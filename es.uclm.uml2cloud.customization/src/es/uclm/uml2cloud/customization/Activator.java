638b2a2641878eb5ff6a82ad8c59dee038484a6d
AdriánBernal <a2b1987@gmail.com>
2018-04-06 15:14:44 +0200

    Changes in customization plugin

---
+++ es.uclm.uml2cloud.customization/src/es/uclm/uml2cloud/customization/Activator.java
@@ -0,0 +1,13 @@
+package es.uclm.uml2cloud.customization;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "es.uclm.uml2cloud.customization"; //$NON-NLS-1$
+
+}
